c23a4eb2c7ec89021ec4d9cd77d89f9db8af998c
Margarita Balkheeva <balheevam@gmail.com>
2023-09-30 16:50:32 +0400

    tests added function renamed

---
+++ src/index.test.js
@@ -0,0 +1,71 @@
+import { expect, it, describe } from 'vitest'
+import { keepUnchangedValues } from "./index";
+
+describe('mergePreferLeft arrays', () => {
+  it('should handle arrays with primitives. If left and right values are arrays, it should apply right', () => {
+    const left = [1, 2, 3];
+    const right = [2, 3, 4];
+    const expected = [2, 3, 4];
+    expect(keepUnchangedValues(left, right)).toEqual(expected)
+  })
+
+  it('should handle arrays with objects. applies right array, but keeps left array objects', () => {
+    const left = [{a: 1}, {b: 2}, {c: 3}, 5];
+    const right = [{b: 2}, 'jjj', {c: 3}, {d: 4}];
+
+    const result = keepUnchangedValues(left, right)
+    const expected = [{b: 2}, 'jjj', {c: 3}, {d: 4}];
+
+    expect(result).toEqual(expected)
+    expect(result[0] === left[1] && result[2] === left[2]).toBe(true)
+  })
+})
+
+describe('mergePreferLeft objects', () => {
+  it('should handle objects with primitives. If left and right values are objects, it should apply right', () => {
+    const left = {a: 1, b: 2, c: 3};
+    const right = {b: 2, c: 3, d: 4};
+    const result = keepUnchangedValues(left, right)
+    const expected = {b: 2, c: 3, d: 4};
+    expect(result).toEqual(expected)
+    expect(result.b === left.b && result.c === left.c).toBe(true)
+  })
+
+  it('should handle objects with objects. applies right object, but keeps left object properties', () => {
+    const left = {a: {a: 1}, b: {b: 2}, c: {c: 3}, d: 5};
+    const right = {b: {b: 2}, jjj: 'jjj', c: {c: 3}, d: {d: 4}};
+
+    const result = keepUnchangedValues(left, right)
+    const expected = {b: {b: 2}, jjj: 'jjj', c: {c: 3}, d: {d: 4}};
+
+    expect(result).toEqual(expected)
+    expect(result.b === left.b && result.c === left.c).toBe(true)
+  })
+})
+
+describe('mergePreferLeft primitives', () => {
+  it('should handle primitives. If left and right values are primitives, it should apply right', () => {
+    const left = 1;
+    const right = 2;
+    const result = keepUnchangedValues(left, right)
+    const expected = 2;
+    expect(result).toEqual(expected)
+  })
+
+  it('should handle primitives. If left is null, it should apply right', () => {
+    const left = null;
+    const right = 2;
+    const result = keepUnchangedValues(left, right)
+    const expected = 2;
+    expect(result).toEqual(expected)
+  })
+
+  it('should handle primitives. If right is null, it should apply right', () => {
+    const left = 1;
+    const right = null;
+    const result = keepUnchangedValues(left, right)
+    const expected = null;
+    expect(result).toEqual(expected)
+  })
+
+})
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,20 @@
 import equal from "fast-deep-equal";
 
-export function mergePreferLeft(left, right) {
-  if (equal(left, right)) return left;
-  if (!left) return right;
-  if (Array.isArray(left) && Array.isArray(right)) {
-    const result = [];
+type Value = null | number | string | boolean | object | Value[];
 
+export function keepUnchangedValues(left: Value, right: Value): Value {
+  if (equal(left, right)) return left;
+  if (!left || typeof left !== typeof right || (typeof left !== 'object' && typeof right !== 'object')) return right;
+  if (Array.isArray(left) && Array.isArray(right)) {
+    const result: Value[] = [];
     for (const item of right) {
       const foundItem = left.find(lItem => equal(lItem, item));
       result.push(foundItem !== undefined ? foundItem : item);
     }
-
     return result;
   }
 
-  if ((typeof left !== 'object' && typeof right !== 'object') || left === null || right === null) {
-    return right;
-  }
-
-  const result = { ...right };
+  const result: Value = {...(right as object)};
   let allEqual = true;
 
   for (const key of Object.keys(left)) {
@@ -26,10 +22,10 @@ export function mergePreferLeft(left, right) {
       allEqual = false;
       continue;
     }
-    const res = mergePreferLeft(left[key], right[key]);
+    const res = keepUnchangedValues(left[key], right[key]);
     if (!equal(res, left[key])) allEqual = false;
     result[key] = res;
   }
 
-  return allEqual ? left : result;
+  return allEqual ? (left as object) : result;
 }
